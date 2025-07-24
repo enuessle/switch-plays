1c93ce0e00ffbc9a5964adeeead0cf3dbd7c0def
Ethan Nuessle <ethan.nuessle@gmail.com>
2025-07-24 16:46:31 -0400

    Server Changes

--- src/server.py
+++ src/server.py
@@ -1,9 +1,24 @@
 import socket
+import nxbt
+import time
+import keyboard
+import random
+
 from validation import validateActions
 
 HOST = "0.0.0.0"     # Listen on all interfaces
 PORT = 5001          # Port to listen on (must match sender)
 BUFF_SIZE = 1024
+
+
+def newAction(nx, controller_index, actions, _down=0.1, _up=0.1):
+    nx.press_buttons(controller_index, actions, down=_down, up=_up)
+
+
+
+
+
+
 
 def openServer(host = HOST, port=PORT):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
